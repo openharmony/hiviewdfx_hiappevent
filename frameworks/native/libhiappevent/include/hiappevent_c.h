914c6b88220c19b787fe2cbba639a8b0c6323495
liujiaxing19 <liujiaxing19@huawei.com>
2024-12-20 14:59:20 +0800

    支持调整系统事件订阅触发条件
    Signed-off-by: liujiaxing19 <liujiaxing19@huawei.com>
    Change-Id: Ibb412e3f117b8a5c5303c54f5a07cf521d185e95

--- frameworks/native/libhiappevent/include/hiappevent_c.h
+++ frameworks/native/libhiappevent/include/hiappevent_c.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -178,6 +178,14 @@ bool HiAppEventInnerConfigure(const char* name, const char* value);
 int HiAppEventInnerWrite(const char* domain, const char* name, enum EventType type, const ParamList list);
 
 void ClearData();
+
+HiAppEvent_Config* HiAppEventCreateConfig();
+
+int HiAppEventSetConfigItem(HiAppEvent_Config* config, const char* itemName, const char* itemValue);
+
+int HiAppEventSetEventConfig(const char* name, HiAppEvent_Config* config);
+
+void HiAppEventDestroyConfig(HiAppEvent_Config* config);
 #ifdef __cplusplus
 }
 #endif
