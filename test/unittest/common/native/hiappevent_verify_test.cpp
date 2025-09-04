813dcf3b6327f8f994dd1e838afc460775a64373
liujiaxing2024 <liujiaxing19@huawei.com>
2025-09-04 20:21:21 +0800

    增加分支覆盖率
    Signed-off-by: liujiaxing2024 <liujiaxing19@huawei.com>
    Change-Id: I82a038a2af38ecfdeb54888ad7dc510a61199266

--- test/unittest/common/native/hiappevent_verify_test.cpp
+++ test/unittest/common/native/hiappevent_verify_test.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <iostream>
 
 #include <gtest/gtest.h>
 
@@ -40,12 +39,11 @@ void HiAppEventVerifyTest::SetUp()
 
 /**
  * @tc.name: HiAppEventVerifyTest001
- * @tc.desc: check the param isValid.
+ * @tc.desc: check the param when it is Valid.
  * @tc.type: FUNC
  */
 HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest001, TestSize.Level0)
 {
-    std::cout << "HiAppEventVerifyTest001 start" << std::endl;
     std::string testName = "testName";
     bool isValid = IsValidUserIdValue(testName);
     EXPECT_TRUE(isValid);
@@ -53,43 +51,80 @@ HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest001, TestSize.Level0)
     EXPECT_TRUE(isValid);
     isValid = IsValidWatcherName(testName);
     EXPECT_TRUE(isValid);
+}
 
-    testName = "";
-    isValid = IsValidUserIdValue(testName);
+/**
+ * @tc.name: HiAppEventVerifyTest002
+ * @tc.desc: check the param when it is empty.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest002, TestSize.Level0)
+{
+    std::string testName = "";
+    bool isValid = IsValidUserIdValue(testName);
     EXPECT_FALSE(isValid);
     isValid = IsValidUserPropValue(testName);
     EXPECT_FALSE(isValid);
     isValid = IsValidWatcherName(testName);
     EXPECT_FALSE(isValid);
-
-    isValid = IsValidEventType(0);  // 1-4: value range of event type
-    EXPECT_FALSE(isValid);
-    isValid = IsValidEventType(1);
-    EXPECT_TRUE(isValid);
-    std::cout << "HiAppEventVerifyTest001 end" << std::endl;
 }
 
 /**
- * @tc.name: HiAppEventVerifyTest002
- * @tc.desc: check the VerifyCustomEventParam func.
+ * @tc.name: HiAppEventVerifyTest003
+ * @tc.desc: check the eventType param.
  * @tc.type: FUNC
  */
-HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest002, TestSize.Level0)
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest003, TestSize.Level0)
 {
-    std::cout << "HiAppEventVerifyTest002 start" << std::endl;
+    bool isValid = IsValidEventType(0);  // 1-4: value range of event type
+    EXPECT_FALSE(isValid);
+    isValid = IsValidEventType(1);
+    EXPECT_TRUE(isValid);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest004
+ * @tc.desc: check the VerifyCustomEventParam func when hiappevent is disable.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest004, TestSize.Level0)
+{
     std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
     bool setRes = HiAppEventConfig::GetInstance().SetConfigurationItem("disable", "true");
     EXPECT_TRUE(setRes);
     int result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::ERROR_HIAPPEVENT_DISABLE);
-
     setRes = HiAppEventConfig::GetInstance().SetConfigurationItem("disable", "false");
     EXPECT_TRUE(setRes);
-    event->SetDomain("a%$321");
-    result = VerifyCustomEventParams(event);
-    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_EVENT_DOMAIN);
+}
 
+/**
+ * @tc.name: HiAppEventVerifyTest005
+ * @tc.desc: check the VerifyCustomEventParam func with different domain.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest005, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("a%$321");
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_EVENT_DOMAIN);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest006
+ * @tc.desc: check the VerifyCustomEventParam func with different name.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest006, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
     event->SetDomain("testDomain");
+
+    event->SetName("");
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
+
     event->SetName("123456");
     result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::ERROR_INVALID_EVENT_NAME);
@@ -97,9 +132,21 @@ HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest002, TestSize.Level0)
     event->SetName("testName");
     result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest007
+ * @tc.desc: check the VerifyCustomEventParam func with different size of params.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest007, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
 
     event->AddParam("testParam");
-    result = VerifyCustomEventParams(event);
+    int result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
 
     for (int i = 0; i < 64; i++) {
@@ -107,61 +154,169 @@ HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest002, TestSize.Level0)
     }
     result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::ERROR_INVALID_CUSTOM_PARAM_NUM);
-    std::cout << "HiAppEventVerifyTest002 end" << std::endl;
 }
 
 /**
- * @tc.name: HiAppEventVerifyTest003
- * @tc.desc: check the VerifyAppCustomEventParam func.
+ * @tc.name: HiAppEventVerifyTest008
+ * @tc.desc: check the VerifyAppCustomEventParam func with repeated paramName.
  * @tc.type: FUNC
  */
-HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest003, TestSize.Level0)
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest008, TestSize.Level0)
 {
-    std::cout << "HiAppEventVerifyTest003 start" << std::endl;
     std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
-    bool setRes = HiAppEventConfig::GetInstance().SetConfigurationItem("disable", "false");
-    EXPECT_TRUE(setRes);
     event->SetDomain("testDomain");
     event->SetName("testName");
+
+    event->AddParam("testParam");
     int result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
-
-    event->AddParam("testKey", "testValue\\1\2\b3\f4\n5\r6\t7");
+    event->AddParam("testParam", "testValue");
     result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest009
+ * @tc.desc: check the VerifyAppCustomEventParam func with invalid paramName.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest009, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
+
+    event->AddParam("a%$321");
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_PARAM_NAME);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest010
+ * @tc.desc: check the VerifyAppCustomEventParam func with Invalid string parameter value length.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest010, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
+
+    std::string invalidValue(1025, 'a');
+    event->AddParam("testParam", invalidValue);
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_PARAM_VALUE_LENGTH);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest011
+ * @tc.desc: check the VerifyAppCustomEventParam func with empty strVec.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest011, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
+
+    std::vector<std::string> strs;
+    event->AddParam("testVectorParam", strs);
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest012
+ * @tc.desc: check the VerifyAppCustomEventParam func with strVec item number is over size.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest012, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
+
+    std::vector<std::string> strs(1025, "testValue");
+    event->AddParam("testVectorParam", strs);
+    int result = VerifyCustomEventParams(event);
+    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_PARAM_VALUE_LENGTH);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest013
+ * @tc.desc: check the VerifyAppCustomEventParam func with valid param.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest013, TestSize.Level0)
+{
+    std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
+    event->SetDomain("testDomain");
+    event->SetName("testName");
+
+    event->AddParam("testKey", "testValue\\\1\2\b3\f4\n5\r6\t7");
+    int result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
 
     std::vector<std::string> strs(3, "testStr");
     event->AddParam("testVectorParam", strs);
     result = VerifyCustomEventParams(event);
     EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
-
-    event->AddParam("testParam");
-    result = VerifyCustomEventParams(event);
-    EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
-    event->AddParam("testParam");
-    result = VerifyCustomEventParams(event);
-    EXPECT_EQ(result, 0);
-    std::cout << "HiAppEventVerifyTest003 end" << std::endl;
 }
 
 /**
- * @tc.name: HiAppEventVerifyTest004
- * @tc.desc: check the VerifyAppCustomEventParam func.
+ * @tc.name: HiAppEventVerifyTest014
+ * @tc.desc: check the VerifyAppEvent func with param item number out of range.
  * @tc.type: FUNC
  */
-HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest004, TestSize.Level0)
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest014, TestSize.Level0)
 {
-    std::cout << "HiAppEventVerifyTest004 start" << std::endl;
     std::shared_ptr<AppEventPack> event = std::make_shared<AppEventPack>();
-    bool setRes = HiAppEventConfig::GetInstance().SetConfigurationItem("disable", "false");
-    EXPECT_TRUE(setRes);
     event->SetDomain("testDomain");
     event->SetName("testName");
-    int result = VerifyCustomEventParams(event);
-    EXPECT_EQ(result, ErrorCode::HIAPPEVENT_VERIFY_SUCCESSFUL);
 
-    event->AddParam("a%$321");
-    result = VerifyCustomEventParams(event);
-    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_PARAM_NAME);
-    std::cout << "HiAppEventVerifyTest004 end" << std::endl;
+    std::vector<bool> bs(101, true);
+    std::vector<char> cs(101, 'a');
+    std::vector<int16_t> shs(101, 1);
+    std::vector<int> is(101, 1);
+    std::vector<int64_t> lls(101, 1);
+    std::vector<float> fs(101, 1.1);
+    std::vector<double> ds(101, 1.1);
+    std::vector<std::string> strs(101, "testStr");
+
+    event->AddParam("testBoolVec", bs);
+    event->AddParam("testCharVec", cs);
+    event->AddParam("testInt16Vec", shs);
+    event->AddParam("testIntVec", is);
+    event->AddParam("testInt64Vec", lls);
+    event->AddParam("testFloatVec", fs);
+    event->AddParam("testDoubleVec", ds);
+    event->AddParam("testStrVec", strs);
+    int result = VerifyAppEvent(event);
+    EXPECT_EQ(result, ErrorCode::ERROR_INVALID_LIST_PARAM_SIZE);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest015
+ * @tc.desc: check the IsApp func.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest015, TestSize.Level0)
+{
+    bool isApp = IsApp();
+    EXPECT_FALSE(isApp);
+}
+
+/**
+ * @tc.name: HiAppEventVerifyTest016
+ * @tc.desc: check the IsValidProcessorName func.
+ * @tc.type: FUNC
+ */
+HWTEST_F(HiAppEventVerifyTest, HiAppEventVerifyTest016, TestSize.Level0)
+{
+    EXPECT_TRUE(IsValidProcessorName("name"));
+    EXPECT_TRUE(IsValidProcessorName("_name"));
+    EXPECT_TRUE(IsValidProcessorName("$name"));
+    EXPECT_FALSE(IsValidProcessorName("1name"));
+    EXPECT_TRUE(IsValidProcessorName("n_ame"));
+    EXPECT_TRUE(IsValidProcessorName("n$ame"));
 }
